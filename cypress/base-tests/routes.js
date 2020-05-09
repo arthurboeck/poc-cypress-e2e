de3f35d45e73d575b941caae826e0addd1d35650
Arthur Guterres Boeck <arthur_boeck@sicredi.com.br>
2020-05-09 13:17:14 -0300

    :boom: add route faker rest api

--- cypress/base-tests/routes.js
+++ cypress/base-tests/routes.js
@@ -3,3 +3,5 @@ export const url = 'https://www.grocerycrud.com/demo/bootstrap_theme_v4/'
 export const routePostCustomer = 'https://www.grocerycrud.com/demo/bootstrap_theme_v4/update/103'
 
 export const routeGetJobs = 'http://api.dataatwork.org/v1/jobs'
+
+export const routeFakeUsersApi = 'https://fakerestapi.azurewebsites.net/api/Users'
