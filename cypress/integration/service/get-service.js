6489861ebc6a3751b72ac2cbafcb996f9fddf87a
Arthur Guterres Boeck <arthur_boeck@sicredi.com.br>
2020-05-09 13:17:49 -0300

    :recycle: refactor get tests

--- cypress/integration/service/get-service.js
+++ cypress/integration/service/get-service.js
@@ -1,31 +1,58 @@
-import { routeGetJobs, } from "../../base-tests/routes";
+import { routeGetJobs, routeFakeUsersApi } from "../../base-tests/routes";
 
-describe('Services Get', () => {
+describe('Get Services', () => {
 
     beforeEach(() => {
         cy.consoleErrosOff();
     });
 
-    it('Get Jobs List', () => {
+    it('Get Jobs List Then Get Job by ID', () => {
 
         cy.server()
         cy.request('GET', routeGetJobs).as('getJobs')
-        cy.get('@getJobs').then((response) => { 
+        cy.get('@getJobs').then((response) => {
             expect(response.status).to.eq(200)
 
             let idJob = response.body[2].uuid
 
             cy.server()
             cy.request({
-            method: 'GET', 
-            url: routeGetJobs+"/"+idJob,
-            // auth:{ -- Parameter used to set bearer token
-            //     bearer:getTokenId()
-            // }
+                method: 'GET',
+                url: routeGetJobs + "/" + idJob,
+                // auth:{ -- Parameter used to set bearer token
+                //     bearer:getTokenId()
+                // }
             }).then((response => {
                 expect(response.status).to.eq(200)
                 expect(response.body).property('uuid').to.eq(idJob)
-        }))
+            }))
+        })
     })
+
+    it('Get User List', () => {
+
+        cy.server()
+        cy.request('GET', routeFakeUsersApi).as('getUsers')
+        cy.get('@getUsers').then((response) => {
+            expect(response.status).to.eq(200)
+        })
+    })
+
+    it('Get Single User', () => {
+
+        var id = 1
+        var username = "User 1"
+        var password = "Password1"
+
+        cy.server()
+        cy.request({
+            method: 'GET',
+            url: routeFakeUsersApi + "/" + id,
+        }).then((response => {
+            expect(response.status).to.eq(200)
+            expect(response.body).property('ID').to.eq(id)
+            expect(response.body).property('UserName').to.eq(username)
+            expect(response.body).property('Password').to.eq(password)
+        }))
     })
 });
