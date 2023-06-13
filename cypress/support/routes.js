29eccd0fb4ffcdd406d503b74a334c64ab917f1c
Arthur Boeck <10471827+arthurboeck@users.noreply.github.com>
2023-06-13 01:10:18 +0000

    update route for intercept

--- cypress/support/routes.js
+++ cypress/support/routes.js
@@ -1,6 +1,6 @@
 //ROUTES CONSTANTS
 export const url = 'https://www.grocerycrud.com/v1.x/demo/bootstrap_theme_v4'
-export const routePostCustomer = '/update/*'
+export const routePostCustomer = '**/update/*'
 
 // export const routeGetJobs = 'http://api.dataatwork.org/v1/jobs'
 
