b429d60d4059265c833855423bba9136bafda18e
Arthur Boeck <10471827+arthurboeck@users.noreply.github.com>
2023-06-13 00:45:17 +0000

    update routes

--- cypress/support/routes.js
+++ cypress/support/routes.js
@@ -1,7 +1,7 @@
 //ROUTES CONSTANTS
-export const url = 'https://www.grocerycrud.com/demo/bootstrap_theme_v4/'
-export const routePostCustomer = 'https://www.grocerycrud.com/demo/bootstrap_theme_v4/update/103'
+export const url = 'https://www.grocerycrud.com/v1.x/demo/bootstrap_theme_v4'
+export const routePostCustomer = 'https://www.grocerycrud.com/v1.x/demo/bootstrap_theme_v4/update/103'
 
 export const routeGetJobs = 'http://api.dataatwork.org/v1/jobs'
 
-export const routeFakeUsersApi = 'https://fakerestapi.azurewebsites.net/api/Users'
+export const routeFakeUsersApi = 'https://fakerestapi.azurewebsites.net/api/v1/Users'
