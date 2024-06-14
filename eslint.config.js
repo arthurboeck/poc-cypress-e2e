34dbc119652d66ecf6c93f14ac4824bb2b1f87cd
Arthur Boeck <arthurguterres@outlook.com>
2024-06-14 19:10:35 -0300

    config eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,28 @@
-import globals from "globals";
+import globals from 'globals';
 import pluginCypress from 'eslint-plugin-cypress/flat';
-import pluginJs from "@eslint/js";
+import pluginJs from '@eslint/js';
 import pluginMocha from 'eslint-plugin-mocha';
 
 export default [
-    pluginJs.configs.all,
-    pluginCypress.configs.globals,
-    pluginMocha.configs.flat.all,
-    {
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.node
-            }
-        }
-    }
+  pluginJs.configs.recommended,
+  pluginCypress.configs.globals,
+  pluginMocha.configs.flat.recommended,
+  {
+    ignores: ['cypress.config.js', 'cypress/plugins/index.js'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    rules: {
+      'func-names': ['off'],
+      'mocha/no-hooks': 'off',
+      'prefer-arrow-callback': 'off',
+    },
+  },
 ];
